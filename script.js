b79a7f92b6a180512139b8c3b324c26b59b94e94
SAM <shaefulamin04@gmail.com>
2023-08-30 22:04:42 +0700

    feat: burger adding button for smaller screen

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 document.querySelector(".year-copyright").textContent =
   new Date().getFullYear() + "©";
+((burger) => {
+  burger.addEventListener("click", () =>
+    document.querySelector(".nav-links").classList.toggle("show")
+  );
+})(document.querySelector(".burger-btn"));
